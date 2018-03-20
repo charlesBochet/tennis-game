20e79dab4597bf07b4756ea6a182fda35d59dce7
Charles BOCHET <charlesb@theodo.fr>
2018-03-20 13:45:09 +0100

    Enable points for player A and player B

--- src/Tennis.php
+++ src/Tennis.php
@@ -3,18 +3,53 @@
 namespace Src;
 
 class Tennis {
-    /**
-     * string $score
-     */
-    private $score = '';
+
+    private $_playerPoints = [
+        0 => '0',
+        1 => '0'
+    ];
 
     /**
      * @return string
      */
     function getScore() {
-        return $this->score;
+        return 'Points: '.$this->_playerPoints[0].' - '.$this->_playerPoints[1].' / Games: 0 - 0';
     }
 
+    private function _playerScores($playerName) {
+        switch($this->_playerPoints[$playerName]) {
+            case '0':
+                $this->_playerPoints[$playerName] = '15';
+                break;
+            case '15':
+                $this->_playerPoints[$playerName] = '30';
+                break;
+            case '30':
+                $this->_playerPoints[$playerName] = '40';
+                break;
+            case '40':
+                if($this->_playerPoints[!$playerName] !== '40') {
+                    $this->_playerPoints[$playerName] = '0';
+                    $this->_playerPoints[!$playerName] = '0';
+                } else {
+                    $this->_playerPoints[$playerName] = 'A';
+                }
+                break;
+        }
+    }
+
+    function playerAScores() {
+        $this->_playerScores(0);
+
+    }
+
+    function playerBScores() {
+        $this->_playerScores(1);
+    }
+
+    /**
+     * @return void
+     */
     function printScore() {
         echo 'Le score actuel est de : '.$this->getScore();
     }
--- tests/TennisTest.php
+++ tests/TennisTest.php
@@ -9,10 +9,64 @@ use PHPUnit\Framework\TestCase;
 
 class TennisTest extends TestCase
 {
+    /**
+     * @var Tennis
+     */
+    protected $tennis;
+
+    protected function setUp()
+    {
+        $this->tennis = new Tennis();
+    }
+
     public function testEmptyScore()
     {
-        $tennis = new Tennis();
-
-        $this->assertSame('', $tennis->getScore());
+        $this->assertSame('Points: 0 - 0 / Games: 0 - 0', $this->tennis->getScore());
     }
+
+    public function testPlayerAScores()
+    {
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 15 - 0 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 30 - 0 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 40 - 0 / Games: 0 - 0', $this->tennis->getScore());
+    }
+
+    public function testPlayersScores()
+    {
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 15 - 0 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 30 - 0 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: 40 - 0 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerBScores();
+        $this->assertSame('Points: 40 - 15 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerBScores();
+        $this->assertSame('Points: 40 - 30 / Games: 0 - 0', $this->tennis->getScore());
+
+        $this->tennis->playerBScores();
+        $this->assertSame('Points: 40 - 40 / Games: 0 - 0', $this->tennis->getScore());
+    }
+
+    public function testEqualityScores() {
+        $this->tennis->playerAScores();
+        $this->tennis->playerAScores();
+        $this->tennis->playerAScores();
+        $this->tennis->playerBScores();
+        $this->tennis->playerBScores();
+        $this->tennis->playerBScores();
+        $this->tennis->playerAScores();
+        $this->assertSame('Points: A - 40 / Games: 0 - 0', $this->tennis->getScore());
+    }
+
+
 }
